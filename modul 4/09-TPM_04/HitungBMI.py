bb9ae2555f2e3e24c7f74c5a15dcf260602277f5
Fahmi-Az <angkatandualima25@gmail.com>
2024-12-04 20:52:08 +0800

    Add files via upload

---
+++ modul 4/09-TPM_04/HitungBMI.py
@@ -0,0 +1,50 @@
+# menghitung BMI 
+
+# input data tetap
+# Data tetap
+berat = 55  # berat badan dalam kilogram
+tinggi = 171  # tinggi badan dalam sentimeter
+
+# Mengonversi tinggi dari cm ke meter
+tinggi = tinggi / 100
+
+# Menghitung BMI
+bmi = berat / (tinggi ** 2)
+
+# Menentukan kategori BMI
+if bmi < 18.5:
+    kategori = "Kekurangan berat badan"
+elif 18.5 <= bmi < 24.9:
+    kategori = "Normal"
+elif 25 <= bmi < 29.9:
+    kategori = "Kelebihan berat badan"
+else:
+    kategori = "Obesitas"
+
+# Menampilkan hasil
+print(f"skor BMI Anda adalah: {bmi:.2f}")
+print(f"Kategori: {kategori}")
+
+# Input data user
+berat = float(input("Masukkan berat badan Anda (kg): "))
+tinggi = float(input("Masukkan tinggi badan Anda (cm): "))
+
+# Mengonversi tinggi dari cm ke meter
+tinggi = tinggi / 100
+
+# Menghitung BMI
+bmi = berat / (tinggi ** 2)
+
+# Menentukan kategori BMI
+if bmi < 18.5:
+    kategori = "Kekurangan berat badan"
+elif 18.5 <= bmi < 24.9:
+    kategori = "Normal"
+elif 25 <= bmi < 29.9:
+    kategori = "Kelebihan berat badan"
+else:
+    kategori = "Obesitas"
+
+# Menampilkan hasil
+print(f"BMI Anda adalah: {bmi:.2f}")
+print(f"Kategori: {kategori}")
